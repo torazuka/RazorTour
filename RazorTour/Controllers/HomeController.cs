496b45f29549dd56c8ef63af0a8c801ddc039a7a
torazuka <torazuka@gmail.com>
2013-08-08 11:11:09 +0900

    added a controller

---
+++ RazorTour/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using RazorTour.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorTour.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product { 
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+    }
+}
