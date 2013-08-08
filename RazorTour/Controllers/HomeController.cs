fa8c3bd4e3f484442f312767cb714b1da383fee3
torazuka <torazuka@gmail.com>
2013-08-08 17:49:08 +0900

    added code to enumerate an array

--- RazorTour/Controllers/HomeController.cs
+++ RazorTour/Controllers/HomeController.cs
@@ -34,5 +34,15 @@ namespace RazorTour.Controllers
 
             return View(myProduct);
         }
+
+        public ActionResult DemoArray(){
+            Product[] array = {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+            return View(array);
+        }
     }
 }
